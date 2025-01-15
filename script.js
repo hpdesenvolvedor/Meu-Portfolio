562d477f6e0460d13c82de798c255f2c0d708079
Raphael <amvrapha@gmail.com>
2025-01-15 14:16:58 -0300

    Initial commit with portfolio website

---
+++ script.js
@@ -0,0 +1,145 @@
+
+
+const translations = {
+  en: {
+      "translate-button": "Translate",
+      "logo-text": "Raphael's<span> Portfolio</span>",
+      "nav-home": "Home",
+      "nav-about": "About",
+      "nav-services": "Services",
+      "nav-contact": "Contact",
+      "contact-me": "Contact Me",
+      "home-title": "Hi, It's <span>Raphael</span>",
+      "home-subtitle": "I'm a <span>Web Developer</span>",
+      "home-description": "Specialized in crafting complete digital experiences, combining skills in UI/UX design and interface prototyping to deliver intuitive and functional solutions. My work ranges from user-centered planning and design to the development of scalable web applications, focusing on high performance and responsiveness. I strive to align creativity, technology, and best development practices to efficiently meet project needs.",
+      "hire": "Hire",
+      "contact": "Contact",
+      "about-title": "About <span>Me</span>",
+      "about-description": "Software development professional with three years of experience at a multinational company with diversified operations in the Oil & Gas and Satellite/Internet sectors, where I worked on challenging projects with a focus on UI/UX design. In the Satellite/Internet sector, I participated in the development of innovative solutions for tracking and monitoring Wi-Fi signals in buses and vans, optimizing connectivity and user experience during transportation. My responsibilities included web application development, creating scalable and responsive interfaces, using technologies such as GitLab for version control, TypeScript for static typing, React and Next.js for the frontend, Node.js and Prisma for the backend, Figma for interface design, TailwindCSS for styling, and Django for web development. One of my main projects was the implementation and updating of the company's Marketplace to a new design standard, which involved refactoring existing components, optimizing the code for better performance, and creating and editing plugins to add new features and keep the website constantly evolving. I possess solid skills in scalable web application development, REST APIs, Agile (Scrum) methodologies, and UI/UX design. Furthermore, I have experience using English daily while working with global teams, which allowed me to collaborate effectively on international projects.",
+      "read-less-btn": "Read Less",
+      "read-more-btn": "Read More",
+      "services-title": "Services",
+      "ui-ux-design": "UI / Ux <br>Design",
+      "ui-ux-description": "I transform ideas into complete digital experiences, creating interfaces that combine functionality and aesthetics. My work involves designing and testing components focused on user experience, ensuring responsive, intuitive, and accessible design. I have the skills to create everything from simple pages to more complex structures, always prioritizing efficient navigation and an attractive visual layout.",
+      "frontend-development": "Frontend Development",
+      "frontend-description": "Specialist in developing modern and scalable interfaces, I offer solutions to enhance websites and systems by fixing bugs, implementing new features, and optimizing performance. I create custom components and complete applications, ensuring a balance between clean design and high functionality, as well as seamless integration with APIs.",
+      "backend-development": "Backend Development",
+      "backend-description": "With experience in languages such as Java and C, I work on bug fixing and integrating the backend with the frontend, ensuring efficient communication between system components. I focus on maintaining stable, optimized applications ready to handle the demands of robust and dynamic projects.",
+      "footer-faq": "FAQ",
+      "footer-services": "Services",
+      "footer-about-me": "About Me",
+      "footer-contact": "Contact",
+      "footer-copyright": "© Raphael Moreira | All Rights Reserved", // Tradução em inglês
+      "contact-heading": "Contact <span>Me</span>",
+      "send-message-btn": "Send Message",
+      "full-name": "Full Name",
+      "email": "Email",
+      "phone-number": "Phone Number",
+      "subject": "Subject",
+      "message": "Your Message",
+    },
+  pt: {
+      "translate-button": "Traduzir",
+      "logo-text": "Raphael's<span> Portfólio</span>",
+      "nav-home": "Tela Inicial",
+      "nav-about": "Sobre",
+      "nav-services": "Serviços",
+      "nav-contact": "Contato",
+      "contact-me": "Contate-me",
+      "home-title": "Olá, me chamo <span>Raphael</span>",
+      "home-subtitle": "Sou um <span>Desenvolvedor Web</span>",
+      "home-description": "Especializado em criar experiências digitais completas, combinando habilidades em design UI/UX e prototipagem de interfaces para oferecer soluções intuitivas e funcionais. Meu trabalho varia desde o planejamento e design centrados no usuário até o desenvolvimento de aplicações web escaláveis, focando em alta performance e responsividade. Esforço-me para alinhar criatividade, tecnologia e as melhores práticas de desenvolvimento para atender eficientemente às necessidades dos projetos.",
+      "hire": "Contratar",
+      "contact": "Contato",
+      "about-title": "Sobre <span>Mim</span>",
+      "about-description": "Profissional com três anos de experiência em desenvolvimento de software em uma multinacional com atuação diversificada nos setores de Óleo e Gás e Satélites/Internet, onde trabalhei em projetos desafiadores com foco em design UI/UX. No setor de Satélites/Internet, participei do desenvolvimento de soluções inovadoras para tracking e monitoramento de sinais Wi-Fi em ônibus e vans de viagem, otimizando a conectividade e a experiência do usuário durante o transporte. Minhas responsabilidades incluíam o desenvolvimento de aplicações web, a criação de interfaces escaláveis e responsivas, utilizando tecnologias como GitLab para controle de versão, TypeScript para tipagem estática, React e Next.js para o frontend, Node.js e Prisma para o backend, Figma para design de interfaces, TailwindCSS para estilização e Django para desenvolvimento web. Um dos meus principais projetos foi a implementação e atualização do Marketplace da empresa para um novo padrão de design, o que envolveu a refatoração de componentes existentes, a otimização do código para melhor performance e a criação e edição de plugins para adicionar novas funcionalidades e manter o site em constante evolução. Possuo habilidades sólidas em desenvolvimento de aplicações web escaláveis, APIs REST, metodologias ágeis (Scrum) e design UI/UX. Além disso, tenho experiência em utilizar a língua inglesa no dia a dia do trabalho com equipes globais, o que me permitiu colaborar efetivamente em projetos internacionais.",
+      "read-less-btn": "Leia Menos",
+      "read-more-btn": "Leia Mais",
+      "services-title": "Serviços",
+      "ui-ux-design": "UI / Ux <br>Design",
+      "ui-ux-description": "Transformo ideias em experiências digitais completas, criando interfaces que aliam funcionalidade e estética. Meu trabalho envolve projetar e testar componentes focados na experiência do usuário, garantindo design responsivo, intuitivo e acessível. Tenho habilidades para criar desde páginas simples até estruturas mais complexas, sempre priorizando uma navegação eficiente e visual atrativo.",
+      "frontend-development": "Desenvolvimento Frontend",
+      "frontend-description": "Especialista em desenvolvimento de interfaces modernas e escaláveis, ofereço soluções para aprimorar sites e sistemas, corrigindo erros, implementando novas funcionalidades e otimizando a performance. Crio componentes personalizados e aplicações completas, garantindo um equilíbrio entre design limpo e alta funcionalidade, além de integração fluida com APIs.",
+      "backend-development": "Desenvolvimento Backend",
+      "backend-description": "Com experiência em linguagens como Java e C, atuo na correção de bugs e na integração do backend com o frontend, assegurando a comunicação eficiente entre as partes do sistema. Foco em manter aplicações estáveis, otimizadas e prontas para lidar com as demandas de projetos robustos e dinâmicos.",    
+      "footer-faq": "Perguntas Frequentes",
+      "footer-services": "Serviços",
+      "footer-about-me": "Sobre Mim",
+      "footer-contact": "Contato",
+      "footer-copyright": "© Raphael Moreira | Todos os Direitos Reservados", // Tradução em português
+      "contact-heading": "Contate <span>Me</span>",
+      "send-message-btn": "Enviar Mensagem",
+      "full-name": "Nome Completo",
+      "email": "Email",
+      "phone-number": "Número de Telefone",
+      "subject": "Assunto",
+      "message": "Sua Mensagem",
+    },
+};
+
+document.getElementById("translate-btn").addEventListener("click", () => {
+  const elements = document.querySelectorAll("[data-translate-key]");
+  const currentLang = document.documentElement.lang;
+  const newLang = currentLang === "en" ? "pt" : "en";
+
+  elements.forEach((element) => {
+    const key = element.getAttribute("data-translate-key");
+    if (translations[newLang] && translations[newLang][key]) {
+      if (element.placeholder !== undefined) { // Check if it's an element with placeholder
+        element.placeholder = translations[newLang][key];
+      }
+       else if (element.tagName === 'INPUT' && element.type === 'submit') {
+      element.value = translations[newLang][key];
+  }
+       else {
+        element.innerHTML = translations[newLang][key];
+      }
+    }
+  });
+
+  document.documentElement.lang = newLang;
+  localStorage.setItem('lang', newLang);
+});
+
+document.documentElement.lang = "en";
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const readMoreBtn = document.getElementById("read-more-btn");
+  const aboutText = document.getElementById("about-text");
+
+  if (readMoreBtn && aboutText) {
+      let isExpanded = false; // Variável para controlar o estado (expandido ou recolhido)
+      let originalTextHeight = aboutText.scrollHeight;
+      aboutText.style.maxHeight = "100px";
+      aboutText.style.overflow = "hidden";
+      readMoreBtn.addEventListener("click", function(event) {
+          event.preventDefault(); // Evita o comportamento padrão do link
+
+          if (isExpanded) {
+              aboutText.style.maxHeight = "100px";
+              readMoreBtn.textContent = translations[document.documentElement.lang]["read-more-btn"];
+
+          } else {
+              aboutText.style.maxHeight = originalTextHeight + "px";
+              readMoreBtn.textContent = translations[document.documentElement.lang]["read-less-btn"];
+          }
+
+          isExpanded = !isExpanded;
+      });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let menuIcon = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  if (menuIcon && navbar) {
+      menuIcon.onclick = () => {
+          menuIcon.classList.toggle('bx-x');
+          navbar.classList.toggle('active');
+      };
+  } else {
+      console.error("Elementos menu-icon ou navbar não encontrados!");
+  }
+});
